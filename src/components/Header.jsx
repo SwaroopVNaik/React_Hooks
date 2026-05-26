57c7b9ba7df228756107ed285a6787058005bc83
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-05-26 10:37:49 +0530

    component

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useMemo } from 'react';
+
+const Header = () => {
+    
+    console.log("Header Rendered");
+    
+  return <div>
+
+    <h2>Header</h2>
+
+  </div>
+
+}
+
+export default React.memo(Header);
