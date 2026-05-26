ae12de4743515ffaf72b42323109dfab42eb0770
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-05-26 10:37:27 +0530

    Root Component

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React  from "react";
+import Use_State from "./React_Hooks/Usestate/Use_State";
+import Use_state_Multi from "./React_Hooks/Usestate/Use_State_Multi";
+import Use_state_Multi2 from "./React_Hooks/Usestate/Use_State_Multi2";
+import useeffect from "./React_Hooks/UseEffect/Effect";
+import Effect from "./React_Hooks/UseEffect/Effect";
+import Ref from "./React_Hooks/useRef/Ref";
+import Ref2 from "./React_Hooks/useRef/Ref2";
+import Memo from "./React_Hooks/UseMemo/Memo";
+import Callback from "./React_Hooks/useCallBack/Callback";
+import Header from "./components/Header";
+
+let App = () => {
+
+  return <div>
+
+    <Memo />
+
+  </div>
+
+}
+
+export default App; 
