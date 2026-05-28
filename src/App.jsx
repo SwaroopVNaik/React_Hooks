7f7939ecb10d2838b457ed493583bd4e86d2e993
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-05-28 14:55:16 +0530

    App and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import React  from "react";
-import Use_State from "./React_Hooks/Usestate/Use_State";
-import Use_state_Multi from "./React_Hooks/Usestate/Use_State_Multi";
-import Use_state_Multi2 from "./React_Hooks/Usestate/Use_State_Multi2";
-import useeffect from "./React_Hooks/UseEffect/Effect";
-import Effect from "./React_Hooks/UseEffect/Effect";
-import Ref from "./React_Hooks/useRef/Ref";
-import Ref2 from "./React_Hooks/useRef/Ref2";
-import Memo from "./React_Hooks/UseMemo/Memo";
-import Callback from "./React_Hooks/useCallBack/Callback";
-import Header from "./components/Header";
+import Red from "./React_Hooks/useReducer/Red";
 
 let App = () => {
 
   return <div>
-
-    <Memo />
-
-  </div>
+            <Red/>
+         </div>
 
 }
 
-export default App; 
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
+import ContextProvider from './React_Hooks/useContext/AppContext.jsx'
 createRoot(document.getElementById('root')).render(
-
-    <App />
-
+    <ContextProvider>
+      <App />
+    </ContextProvider>
 )
