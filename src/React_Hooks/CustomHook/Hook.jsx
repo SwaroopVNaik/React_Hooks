3fe3f959327a603484bb5ff17cc3b905ff3dd20f
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-05-29 13:50:55 +0530

    custom Hook

--- src/React_Hooks/CustomHook/Hook.jsx
+++ src/React_Hooks/CustomHook/Hook.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import useLocalStorage from "../../Hooks/useLocalStorage";
 
 const Hook = () => {
 
+    const [name, setName] = useLocalStorage('username', "")
+    const [id, setID] = useLocalStorage('userid', "")
+
     return <div>
-        <h1>Custom Hook Pending</h1>
+        <input type="text" 
+        placeholder="Enter Your Name"
+        value = {name} 
+        onChange={(e) => setName(e.target.value)} 
+        />
+
+        <h2>Hello, {name} !</h2>
+
+        <input type="text" 
+        placeholder="Enter Your ID"
+        value = {id} 
+        onChange={(e) => setID(e.target.value)} 
+        />
+
+        <h2>Your ID : {id}</h2>
+
     </div>
 
 }
 
-export default Hook;
+export default Hook; 
